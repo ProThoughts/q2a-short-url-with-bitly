48fd0f7d4d5b1ad99da8a91d6c47a9f135a7c6c3
Jonathan de Montalembert <feunetre@hotmail.com>
2012-02-07 19:41:06 +0800

    final version

--- qa-short-url-layer.php
+++ qa-short-url-layer.php
@@ -9,11 +9,19 @@ class qa_html_theme_layer extends qa_html_theme_base {
 
 	function q_view_main($q_main){
 		qa_html_theme_base::q_view_main($q_main);
-		if(qa_opt('short_url_content_on') && $_SERVER['REMOTE_ADDR'] != '127.0.0.1')
+		if(qa_opt('short_url_content_on')/* && $_SERVER['REMOTE_ADDR'] != '127.0.0.1'*/)
 		{
 			$login = qa_opt('short_url_bitly_username');
 			$api_key = qa_opt('short_url_bitly_api_key');
-			$url = 'dfdf';
+			$url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+			$api_url =  "http://api.bit.ly/v3/shorten?login=".$login."&apiKey=".$api_key."&uri=".$url."&format=txt";
+			if(qa_opt($url))
+				$short_url = qa_opt($url);
+			else{
+				$short_url = file_get_contents($api_url);
+				qa_opt($url, $short_url);
+			}
+				$this->output($short_url);
 		}
 	}
 }
